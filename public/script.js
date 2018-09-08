c4ade5edf6e034adf82d5f2b38dea216446db6e6
eliteboygithub <eboyzhaoyun@hotmail.com>
2018-09-08 13:33:20 +0800

    first modify

--- public/script.js
+++ public/script.js
@@ -13,7 +13,8 @@ new Vue({
     },
     methods: {
         addItem: function (index) {
-            console.log(index);
+            this.total += 9.99;
+            this.cart.push(this.items[index]);
         }
     }
 });
